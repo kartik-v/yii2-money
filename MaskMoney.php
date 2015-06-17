3c80908ed0d3ea1589067f8acea8c4d981706e0c
Kartik Visweswaran <kartikv2@gmail.com>
2015-06-17 22:18:26 +0530

    Update to release v1.2.1

--- MaskMoney.php
+++ MaskMoney.php
@@ -1,9 +1,9 @@
 <?php
 
 /**
- * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014
+ * @copyright Copyright &copy; Kartik Visweswaran, Krajee.com, 2014 - 2015
  * @package yii2-money
- * @version 1.2.0
+ * @version 1.2.1
  */
 
 namespace kartik\money;

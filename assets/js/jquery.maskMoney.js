6b1bad400e1b89774329fcd8f577d9a1622509d5
Jesús Ayala <jayala08@gmail.com>
2015-07-14 00:55:53 -0400

    Fix the buggy unmasked function
    I had a lot of trouble with my country's currency (precision 0 and point as decimal separator) and the unmasked function. When I entered any value greater than 999 the unmasked function returned bad values.
    Made some corrections and now it works perfectly for any currency.
    Test it and you'll see.

--- assets/js/jquery.maskMoney.js
+++ assets/js/jquery.maskMoney.js
@@ -43,20 +43,12 @@
         unmasked : function () {
             return this.map(function () {
                 var value = ($(this).val() || "0"),
-                    isNegative = value.indexOf("-") !== -1,
-                    decimalPart;
-                // get the last position of the array that is a number(coercion makes "" to be evaluated as false)
-                $(value.split(/\D/).reverse()).each(function (index, element) {
-                    if(element) {
-                        decimalPart = element;
-                        return false;
-                   }
-                });
-                value = value.replace(/\D/g, "");
-                value = value.replace(new RegExp(decimalPart + "$"), "." + decimalPart);
-                if (isNegative) {
-                    value = "-" + value;
-                }
+                    settings = $(this).data('settings');
+                value = value.replace(settings.prefix, '');
+                value = value.replace(settings.suffix, '');
+                while (value.indexOf(settings.thousands) != -1)
+                    value = value.replace(settings.thousands, '');
+                value = value.replace(settings.decimal, '.');
                 return parseFloat(value);
             });
         },
